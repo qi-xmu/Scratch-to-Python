421fdce90053fab6d15e04125f23633d8da1f842
Qi <56908202+qi-xmu@users.noreply.github.com>
2021-10-13 18:35:42 +0800

    添加注释

--- python/new/led_example.py
+++ python/new/led_example.py
@@ -1,24 +1,21 @@
 import time
 from module.LED import *
 
-# 关于点灯的部分
+# 1 关于点灯的部分
 led1 = LED(3)
-# 点亮led
-led1.turn_on()
-# 熄灭
-led1.turn_off()
-# 设置亮度
-led1.brightness(10)
+led1.turn_on()  # 点亮led
+led1.turn_off()  # 熄灭
+led1.brightness(10)  # 设置亮度
 
 
-# 实现亮灭5次
+# 2 实现亮灭5次
 for i in range(5):
     led1.turn_on()
     time.sleep(1)
     led1.turn_off()
     time.sleep(1)
 
-# 实现渐变灯效果
+# 3 实现渐变灯效果
 step = 1
 liangdu = 0
 
@@ -30,7 +27,7 @@ for i in range(50000):
     time.sleep(0.01)
 
 
-# 按键控制led
+# 4 按键控制led
 led2 = LED(4)
 btn = Button(5)
 btn.bind(led2)
--- python/new/module/Track.py
+++ python/new/module/Track.py
@@ -6,7 +6,7 @@ class Track:
 
     def __init__(self, channels):
         self.channels = channels
-        GPIO.setup(self.channels, GPIO.IN, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.channels, GPIO.IN, initial=GPIO.LOW)
 
     def state(self):
         t1 = GPIO.input(self.channels[0])
@@ -23,22 +23,28 @@ class Track:
         out = [-2, -1, 0, 1, 2]  # 左转 --> 右转
 
         if status[:3] == (0, 0, 1):
-            return out[1]
+            return out[1]  # 小左转
         elif status[1:] == (1, 0, 0):
-            return out[3]
+            return out[3]  # 小右转
 
         elif status[0] == 0:
-            return out[0]
+            return out[0]  # 左转
         elif status[3] == 0:
-            return out[4]
+            return out[4]  # 右转
 
         elif status[1] == 0 and status[2] == 1:
-            return out[1]
+            # or
+            # elif status[1:3] == (0,1)
+            return out[1]  # 小小左转
         elif status[1] == 1 and status[2] == 0:
-            return out[3]
+            # or
+            # elif status[1:3] == (1,0)
+            return out[3]  # 小小右转
 
         elif status[1] == 0 and status[2] == 0:
-            return out[2]
+            # or
+            # elif status[1:3] == (0,0)
+            return out[2]  # 前进
 
     def __def__(self):
         GPIO.cleanup()
